d3ab2ff17274efc3dd6f22e1fe57cadd31453f19
shah911 <mianshahzeb911@gmail.com>
2023-07-28 09:06:43 -0700

    Updated the code with new changes

--- src/app/components/Contact/Contact.jsx
+++ src/app/components/Contact/Contact.jsx
@@ -1,27 +1,32 @@
-'use client'
-import styles from './page.module.css'
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-import CustomButton from '../Custombutton/CustomButton';
+"use client";
+import styles from "./page.module.css";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import CustomButton from "../Custombutton/CustomButton";
 
-export default function Contact({id}) {
-    const ref = useRef();
+export default function Contact({ id }) {
+  const ref = useRef();
   const [success, setSuccess] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const form = ref.current;
-    const name = form['name'].value;
-    const email = form['email'].value;
-    const message = form['message'].value;
+    const name = form["name"].value;
+    const email = form["email"].value;
+    const message = form["message"].value;
 
     if (!name || !email || !message) {
       setErrorMessage(true);
     } else {
       e.preventDefault();
       emailjs
-        .sendForm('service_n5x8q6d', 'template_x64ngce', ref.current, 'PefCKytrFK7i89DkL')
+        .sendForm(
+          "service_n5x8q6d",
+          "template_x64ngce",
+          ref.current,
+          "PefCKytrFK7i89DkL"
+        )
         .then(
           (result) => {
             console.log(result.text);
@@ -43,29 +48,52 @@ export default function Contact({id}) {
         <div className={styles.Left}>
           <form ref={ref} onSubmit={handleSubmit} className={styles.Form}>
             <h1 className={styles.Title}>Contact Me</h1>
-            <input type="text" placeholder="Name" name="name" className={styles.Input} />
-            <input type="text" placeholder="Email" name="email" className={styles.Input} />
-            <textarea placeholder="Write your Message" rows="8" name="message" className={styles.TextArea} />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              className={styles.Input}
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              name="email"
+              className={styles.Input}
+            />
+            <textarea
+              placeholder="Write your Message"
+              rows="8"
+              name="message"
+              className={styles.TextArea}
+            />
             <div className={styles.ButtonContainer}>
               <CustomButton type="submit" title="Send" />
             </div>
-            {success && <span className={styles.Message}>Your message has been sent. We'll get to you soon :)</span>}
-            {errorMessage && <span className={styles.Message}>Please fill all the details :)</span>}
+            {success && (
+              <span className={styles.Message}>
+                Your message has been sent. We'll get to you soon :)
+              </span>
+            )}
+            {errorMessage && (
+              <span className={styles.Message}>
+                Please fill all the details :)
+              </span>
+            )}
           </form>
         </div>
         <div className={styles.Right}>
-        <div className={styles.RightItems}>
-         <h1 className={styles.Title}>Got a Project? Let's Talk</h1>
-          <p className={styles.desc}>
-           Are you ready to start your first project? Enjoy a <strong>15%</strong> discount
-           and hire me <strong>free</strong> for the first week. If you are not satisfied with
-           my work, you can cancel the project within the first week without 
-           incurring any charges. On the other hand, if you find the results 
-           satisfactory, you can proceed with the payment as agreed upon.
-          </p>
-        </div>
+          <div className={styles.RightItems}>
+            <h1 className={styles.Title}>Got a Project? Let's Talk</h1>
+            <p className={styles.desc}>
+              As a field that is constantly evolving, I am excited to learn new
+              technologies and continue developing my skills. If you’re
+              interested in collaborating or discussing web development, feel
+              free to connect with me. I’m passionate about creating something
+              truly exceptional and am always eager to take on a new challenge.
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
